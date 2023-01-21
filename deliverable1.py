1442429e5f275a16f1a4dbe5f9669c3045c12d8c
eroscupid88 <68611769+eroscupid88@users.noreply.github.com>
2023-01-20 22:22:42 -0600

    update

--- deliverable1.py
+++ deliverable1.py
@@ -1,52 +1,55 @@
 import time
 import RPi.GPIO as GPIO
 # set pins GPIO
-lightPins = (12,13,16) # green and red and blue
-buttonPin = 11
+lightPins = (12,13,16) # 12: green ; 13: red ; 16: blue
+buttonPin = 11 
 sensorOutputPin = 40
 light_state = 0
+# initial set up function only call 1
 def setup():
-    
-
-    print("Set Up Phase")
+    print("Set Up Phase"
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(sensorOutputPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
     GPIO.setup(lightPins,GPIO.OUT)
     GPIO.setup(buttonPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(buttonPin, GPIO.BOTH, callback=pressButton,bouncetime = 100)
- #   GPIO.add_event_detect(sensorOutputPin,GPIO.BOTH,callback=sensorDetect,bouncetime = 100)
+    # setup output light turn off when high
     GPIO.output(lightPins, GPIO.HIGH)
+    
+# blinkLight function turn on differents light depend on the state system'
+# param: state: 0 -> when button is pressed, 1 -> when button is not pressed
 def blinkLight(state):
     global light_state
+    # first state: RED LED on
     if ((state == 0 and light_state == 0)or (state == 0 and light_state == 3)):
         GPIO.output(13,GPIO.LOW)
         GPIO.output(12,GPIO.HIGH)
         GPIO.output(16, GPIO.HIGH)
         light_state = 1
-        print('light_state: %s'%light_state)
         return light_state
+    #second state: GREEN LED on
     if (state == 0 and light_state ==1):
         GPIO.output(12,GPIO.LOW)
         GPIO.output(13,GPIO.HIGH)
         GPIO.output(16, GPIO.HIGH)
         light_state = 2
-        print('light_state: %s'%light_state)
         return light_state
+    # third state: BLUE LED on
     if (state== 0 and light_state ==2):
         GPIO.output(16,GPIO.LOW)
         GPIO.output(13,GPIO.HIGH)
         GPIO.output(12, GPIO.HIGH)
         light_state = 3
-        print('light_state: %s'%light_state)
         return light_state
+    # forth state: ALL LED on (this state motion sensor is activated)
     if (state==0 and light_state == 3):
         GPIO.output(16,GPIO.LOW)
         GPIO.output(13,GPIO.LOW)
         GPIO.output(12, GPIO.LOW)
         light_state = 0
-        print('light_state: %s'%light_state)
         return light_state
-
+# onLight function call when sensor is activated
+# params: state: 0 -> sensor detected motion; 1 -> sensor not detected motion
 def onLight(state):
     if(state == 0):
         GPIO.output(13,GPIO.LOW)
@@ -57,13 +60,15 @@ def onLight(state):
         GPIO.output(12,GPIO.HIGH)
         GPIO.output(16, GPIO.HIGH)
  
+# callback function pressButton activate when button is pressed          
 def pressButton(channel):
     print("button has been pressed")
     blinkLight(GPIO.input(buttonPin))
-
+# when sensor is activated, seonsorDetect function is called
 def sensorDetect(channel):
     print("Motion has been detected")
     onLight(GPIO.input(sensorOutputPin))
+#main loop only run on state 4 
 def loop():
     while(True):
         global light_state
@@ -75,17 +80,19 @@ def loop():
         if (light_state == 3 and GPIO.input(sensorOutputPin) == 1):
             GPIO.output(13,GPIO.HIGH)
             GPIO.output(12,GPIO.HIGH)
-            GPIO.output(16, GPIO.HIGH)           
+            GPIO.output(16, GPIO.HIGH)
+        # turn off all light when system state is 0
         if (light_state == 0 ):
             GPIO.output(13,GPIO.HIGH)
             GPIO.output(12,GPIO.HIGH)
             GPIO.output(16, GPIO.HIGH)
         time.sleep(1)
+# destroy function is called when system is interrupted and clean up all preset
 def destroy():
     GPIO.output(lightPins,GPIO.HIGH)
     GPIO.cleanup()
+#initial setup          
 if __name__ == "__main__":
-    
     setup()
     try:
         loop()
